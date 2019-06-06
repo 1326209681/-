d03b39e04492d672dda16ffc6aa3380e1d1933ad
zhang <zhang18181818174@aliyun.com>
2019-06-06 17:37:06 +0800

    add

---
+++ README.md
@@ -0,0 +1,5 @@
+# golden_miner
+js黄金矿工小游戏
+demo演示：http://weijunw.gitee.io/golden_miner
+
+本地文件夹：D:\company-project\node\hjkg\golden_miner
---
+++ github_webpage/js/vuepage.js
@@ -0,0 +1,21 @@
+const wm = new Vue({
+    el: '#app',
+    data: {
+        name: ''
+    },
+    method: {
+
+    },
+    computed: {
+
+    },
+    components: {
+
+    },
+    directives: {
+
+    },
+    watch: {
+        
+    }
+})
